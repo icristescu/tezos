5922033b117567bda839422c1a8d8daecfa31164
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2020-08-28 23:05:36 +0200

    remove duplicates

--- src/proto_006_PsCARTHA/bin_get_contracts/get_contracts.ml
+++ src/proto_006_PsCARTHA/bin_get_contracts/get_contracts.ml
@@ -1,4 +1,5 @@
 module Format' = Format
+module P = Tezos_raw_protocol_006_PsCARTHA
 
 let mainnet_genesis =
   {
@@ -10,6 +11,14 @@ let mainnet_genesis =
       Protocol_hash.of_b58check_exn
         "Ps9mPmXaRzmzk35gbAYNCAw6UXdE2qoABTHbN2oEEc1qM7CwT9P";
   }
+
+module ScriptOrd : Map.OrderedType with type t = P.Script_expr_hash.t = struct
+  type t = P.Script_expr_hash.t
+
+  let compare = P.Script_expr_hash.compare
+end
+
+module Scripts = Map.Make (ScriptOrd)
 
 let () =
   Lwt_main.run
@@ -38,21 +47,29 @@ let () =
       ctxt
     >>= (fun res -> Lwt.return (Environment.wrap_error res))
     >>=? fun raw_ctxt ->
-    Storage.Contract.fold raw_ctxt ~init:() ~f:(fun contract () ->
+    Storage.Contract.fold raw_ctxt ~init:Scripts.empty ~f:(fun contract m ->
         let open Tezos_protocol_environment_006_PsCARTHA.Environment in
         let open Tezos_protocol_environment_006_PsCARTHA.Environment
                  .Error_monad in
         Storage.Contract.Code.get_option raw_ctxt contract
         >>= function
         | Ok (_, None) ->
-            Lwt.return ()
+            Lwt.return m
         | Ok (_, Some code) ->
             let code = Data_encoding.force_decode code in
             let code =
               match code with Some code -> code | None -> assert false
             in
-            Format'.printf "%a" Michelson_v1_printer.print_expr code ;
-            Lwt.return ()
+            let bytes =
+              Data_encoding.Binary.to_bytes_exn Script_repr.expr_encoding code
+            in
+            let key = Script_expr_hash.hash_bytes [bytes] in
+            Lwt.return (Scripts.add key code m)
         | Error _ ->
-            Lwt.return ())
-    >>= fun () -> return_unit )
+            Lwt.return m)
+    >>= fun m ->
+    Scripts.fold
+      (fun hash _script () -> Format'.printf "%a" P.Script_expr_hash.pp hash)
+      m
+      () ;
+    return_unit )
