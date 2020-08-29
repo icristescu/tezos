b1ba8dcbd285993e44f2d9c14596361c4026e63c
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2020-08-29 14:31:03 +0200

    If printing fails, give the list of addresses for the problematic script

--- src/proto_006_PsCARTHA/bin_get_contracts/get_contracts.ml
+++ src/proto_006_PsCARTHA/bin_get_contracts/get_contracts.ml
@@ -65,19 +65,34 @@ let () =
               Data_encoding.Binary.to_bytes_exn Script_repr.expr_encoding code
             in
             let key = Script_expr_hash.hash_bytes [bytes] in
-            Lwt.return (Scripts.add key code m)
+            Lwt.return
+            @@ Scripts.update
+                 key
+                 (function
+                   | Some (code, contracts) ->
+                       Some (code, contract :: contracts)
+                   | None ->
+                       Some (code, [contract]))
+                 m
         | Error _ ->
             Lwt.return m)
     >>= fun m ->
     print_endline "Listing addresses done" ;
     Scripts.fold
-      (fun hash script () ->
+      (fun hash (script, contracts) () ->
         let filename = P.Script_expr_hash.to_b58check hash ^ ".tz" in
         let chan = open_out filename in
         let fmt = Format'.formatter_of_out_channel chan in
-        Format'.fprintf fmt "%a\n" Michelson_v1_printer.print_expr script ;
-        flush chan ;
-        close_out chan)
+        ( try Format'.fprintf fmt "%a\n" Michelson_v1_printer.print_expr script
+          with _ ->
+            Format'.eprintf
+              "Could not print script for %s from contracts %a"
+              filename
+              (Format'.pp_print_list
+                 ~pp_sep:Format'.pp_print_space
+                 P.Contract_repr.pp)
+              contracts ) ;
+        flush chan ; close_out chan)
       m
       () ;
     return_unit )
